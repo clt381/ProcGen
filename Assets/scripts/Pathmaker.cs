631e48d8e67b54a63d0a9ade0491e3d958fd1ac0
Unknown <clt381@nyu.edu>
2017-10-31 10:17:46 -0400

    The Game
    it's done

---
+++ Assets/scripts/Pathmaker.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// MAZE PROC GEN LAB
+// all students: complete steps 1-6, as listed in this file
+// optional: if you have extra time, complete the "extra tasks" to do at the very bottom
+
+// STEP 1: ======================================================================================
+// put this script on a Sphere... it will move around, and drop a path of floor tiles behind it
+
+public class Pathmaker : MonoBehaviour {
+
+    //public int counter = 0;
+    float ranNumber;
+    float ranNumber2;
+    int numberOfFloors;
+    public FloorController fController;
+    //public static List<GameObject> floorList = new List<GameObject>();     //should be static because it will keep track of all pathmakers
+
+
+    public Transform floorPrefab1;
+    public Transform floorPrefab2;
+    public Transform floorPrefab3;
+    public Transform floorPrefab4;
+    public Transform floorPrefab5;
+    public Transform floorPrefab6;
+
+    public Transform pathmakerSpherePrefab;
+
+    void Start()
+    {
+        fController = GameObject.FindGameObjectWithTag("FloorController").GetComponent<FloorController>();
+        numberOfFloors = Random.Range(300, 800);
+    }
+
+
+	void Update () {
+
+        if (fController.counter < numberOfFloors)
+        {
+            ranNumber = Random.Range(0.0f, 1.0f);
+            ranNumber2 = Random.Range(0.0f, 1.0f);
+
+            if (ranNumber < 0.25f)
+            {
+                transform.Rotate(0, 90f, 0);
+            }
+            else if (ranNumber > 0.25f && ranNumber < 0.5f)
+            {
+                transform.Rotate(0, -90f, 0);
+            }
+            else if (ranNumber > 0.95f && ranNumber < 1.0f )
+            {
+                Instantiate(pathmakerSpherePrefab, transform.position, transform.rotation);
+
+            }
+
+            if (ranNumber < 0.1)
+            {
+                var f = Instantiate(floorPrefab1, transform.position, transform.rotation);
+                fController.floorList.Add(f.gameObject);
+            }
+            else if (ranNumber > 0.1 && ranNumber < 0.2)
+            {
+                var f = Instantiate(floorPrefab2, transform.position, transform.rotation);
+                fController.floorList.Add(f.gameObject);
+            }
+            else if (ranNumber > 0.2 && ranNumber < 0.6)
+            {
+                if (ranNumber2 < 0.5)
+                {
+                    var f = Instantiate(floorPrefab3, transform.position, transform.rotation);
+                    fController.floorList.Add(f.gameObject);
+                }
+                else if (ranNumber2 > 0.5)
+                {
+                    var f = Instantiate(floorPrefab4, transform.position, transform.rotation);
+                    fController.floorList.Add(f.gameObject);
+                }
+            }
+            else if (ranNumber > 0.6)
+            {
+                var f = Instantiate(floorPrefab5, transform.position, transform.rotation);
+                fController.floorList.Add(f.gameObject);
+            }
+            //var f = Instantiate(floorPrefab, transform.position, transform.rotation);
+
+            
+            transform.position += transform.forward * 5;
+            fController.counter++;
+                }
+        else {
+            Destroy(gameObject);
+        }
+        //Debug.Log(fController.counter);
+	}
+
+} 
+
+// MORE STEPS BELOW!!!........
+
+
+
+
+// STEP 3: =====================================================================================
+// implement, test, and stabilize the system
+
+//	IMPLEMENT AND TEST:
+//	- save your scene!!! the code could potentially be infinite / exponential, and crash Unity
+//	- put Pathmaker.cs on a sphere, configure all the prefabs in the Inspector, and test it to make sure it works
+//	STABILIZE: 
+//	- code it so that all the Pathmakers can only spawn a grand total of 500 tiles in the entire world; how would you do that?
+//	- (hint: declare a "public static int" and have each Pathmaker check this "globalTileCount", somewhere in your code? if there are already enough tiles, then maybe the Pathmaker could Destroy my game object
+
+
+
+// STEP 4: ======================================================================================
+// tune your values...
+
+// a. how long should a pathmaker live? etc.
+// b. how would you tune the probabilities to generate lots of long hallways? does it work?
+// c. tweak all the probabilities that you want... what % chance is there for a pathmaker to make a pathmaker? is that too high or too low?
+
+
+
+// STEP 5: ===================================================================================
+// maybe randomize it even more?
+
+// - randomize 2 more variables in Pathmaker.cs for each different Pathmaker... you would do this in Start()
+// - maybe randomize each pathmaker's lifetime? maybe randomize the probability it will turn right? etc. if there's any number in your code, you can randomize it if you move it into a variable
+
+
+
+// STEP 6:  =====================================================================================
+// art pass, usability pass
+
+// - move the game camera to a position high in the world, and then point it down, so we can see your world get generated
+// - CHANGE THE DEFAULT UNITY COLORS, PLEASE, I'M BEGGING YOU
+// - add more detail to your original floorTile placeholder -- and let it randomly pick one of 3 different floorTile models, etc. so for example, it could randomly pick a "normal" floor tile, or a cactus, or a rock, or a skull
+//		- MODEL 3 DIFFERENT TILES IN MAYA! DON'T STOP USING MAYA OR YOU'LL FORGET IT ALL
+//		- add a simple in-game restart button; let us press [R] to reload the scene and see a new level generation
+// - with Text UI, name your proc generation system ("AwesomeGen", "RobertGen", etc.) and display Text UI that tells us we can press [R]
+
+
+
+// OPTIONAL EXTRA TASKS TO DO, IF YOU WANT: ===================================================
+
+// DYNAMIC CAMERA:
+// position the camera to center itself based on your generated world...
+// 1. keep a list of all your spawned tiles
+// 2. then calculate the average position of all of them (use a for() loop to go through the whole list) 
+// 3. then move your camera to that averaged center and make sure fieldOfView is wide enough?
+
+// BETTER UI:
+// learn how to use UI Sliders (https://unity3d.com/learn/tutorials/topics/user-interface-ui/ui-slider) 
+// let us tweak various parameters and settings of our tech demo
+// let us click a UI Button to reload the scene, so we don't even need the keyboard anymore!
+
+// WALL GENERATION
+// add a "wall pass" to your proc gen after it generates all the floors
+// 1. raycast downwards around each floor tile (that'd be 8 raycasts per floor tile, in a square "ring" around each tile?)
+// 2. if the raycast "fails" that means there's empty void there, so then instantiate a Wall tile prefab
+// 3. ... repeat until walls surround your entire floorplan
+// (technically, you will end up raycasting the same spot over and over... but the "proper" way to do this would involve keeping more lists and arrays to track all this data)
---
+++ Assets/scripts/PressRToReload.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressRToReload : MonoBehaviour {
+
+    //Scene currentScene;
+    public GameObject pathmakerPrefab;
+    public Transform spawnPoint;
+    public static GameObject[] floors;
+    public Pathmaker pathMaker;
+    public FloorController fController;
+
+	void Start () {
+        //currentScene = SceneManager.GetActiveScene();
+        fController = GameObject.FindGameObjectWithTag("FloorController").GetComponent<FloorController>();
+	}
+	
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //SceneManager.LoadScene(currentScene.name);
+            //Debug.Log("loaded");
+            floors = GameObject.FindGameObjectsWithTag("Ground");
+            foreach (GameObject floor in floors)
+            {
+                Destroy(floor.gameObject);
+                Debug.Log("gameobjects destroyed");
+            }
+            //Invoke("SpawnPathMaker", 0.1f);
+            //Instantiate(pathmakerPrefab, spawnPoint.position, spawnPoint.rotation);
+            fController.counter = 0;
+            StartCoroutine(SpawnPathMaker());
+        }
+	}
+
+    IEnumerator SpawnPathMaker()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Instantiate(pathmakerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Debug.Log("loaded");
+    }
+}
